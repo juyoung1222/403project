22dc478465ea1a3b1da834c1872c701800c28518
juyoung1222 <jjurong1222@gmail.com>
2020-11-23 15:08:44 +0900

    403project

---
+++ shoppingproject/src/test/java/com/edu/project/ShoppingprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
