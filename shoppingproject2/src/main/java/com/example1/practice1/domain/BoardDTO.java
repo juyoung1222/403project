22280b104be33a5fefa3170841d00b9df770531d
juyoung1222 <jjurong1222@gmail.com>
2020-11-26 23:45:49 +0900

    2020Project 11/26

--- shoppingproject2/src/main/java/com/example1/practice1/domain/BoardDTO.java
+++ shoppingproject2/src/main/java/com/example1/practice1/domain/BoardDTO.java
@@ -6,9 +6,7 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-@Getter
-@Setter
-@ToString
+
 public class BoardDTO {
 	private int boardno;//게시글 일련번호
 	private String subject;//게시글 제목
@@ -17,5 +15,53 @@ public class BoardDTO {
 	private Date regdate;//게시글 작성일시
 	
 	public BoardDTO() {}
+
+	public int getBoardno() {
+		return boardno;
+	}
+
+	public void setBoardno(int boardno) {
+		this.boardno = boardno;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Date getRegdate() {
+		return regdate;
+	}
+
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+
+	@Override
+	public String toString() {
+		return "BoardDTO [boardno=" + boardno + ", subject=" + subject + ", writer=" + writer + ", content=" + content
+				+ ", regdate=" + regdate + "]";
+	}
+	
+	
 
 }
